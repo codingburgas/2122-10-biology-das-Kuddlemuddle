701632e3ed99055cb4d88a3c9e9c74c24388394d
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2022-05-07 10:33:05 +0300

    Create view and create Organisation page designs

--- client/ConsoleApplication54/ConsoleApplication54.cpp
+++ client/ConsoleApplication54/ConsoleApplication54.cpp
@@ -222,6 +222,7 @@ RegisterData registerPage()
 	newprof.registerHandler(account);
 	return account;
 }
+
 void accountPage(RegisterData acc)
 {
 	gotoxy(40, 6); color(6); cout << "A C C O U N T     S E T T I N G S"; color(7);
@@ -282,18 +283,18 @@ void adminPage(vector<string> systemOrg, vector<string> systemUser)
 
 	while (true)
 	{
-		if (counter2 == 0) 
+		if (counter2 == 0)
 		{
 			createButton(5, "        O R G A N I Z A T I O N S        ", 41, 15, 6);
-			createButton(5, "            A L L   U S E R S            ", 41, 70, 8,8);
+			createButton(5, "            A L L   U S E R S            ", 41, 70, 8, 8);
 		}
 
 		else
 		{
-			createButton(5, "        O R G A N I Z A T I O N S        ", 41, 15, 8,8);
+			createButton(5, "        O R G A N I Z A T I O N S        ", 41, 15, 8, 8);
 			createButton(5, "            A L L   U S E R S            ", 41, 70, 6);
 		}
-		
+
 		for (int i = 0; i < systemOrg.size(); i++)
 		{
 			counter2 == 0 ? counter == i ? color(6) : color(7) : color(8);
@@ -302,23 +303,23 @@ void adminPage(vector<string> systemOrg, vector<string> systemUser)
 
 		for (int i = 0; i < systemUser.size(); i++)
 		{
-			counter2 == 1 ? counter == i? color(6) : color(7): color(8);
+			counter2 == 1 ? counter == i ? color(6) : color(7) : color(8);
 			gotoxy(70, 8 + i); cout << char(186) << " " << systemUser[i];
 		}
 
 		key = _getch();
 
-		if (key == 72 && (counter >= 1 && counter <= systemOrg.size())) 
+		if (key == 72 && (counter >= 1 && counter <= systemOrg.size()))
 		{
 			counter--;
 		}
 
-		if (key == 80 && (counter >= 0 && counter < systemOrg.size() - 1)) 
+		if (key == 80 && (counter >= 0 && counter < systemOrg.size() - 1))
 		{
 			counter++;
 		}
 
-		if (key == 75 && (counter2 == 1)) 
+		if (key == 75 && (counter2 == 1))
 		{
 			counter2--;
 		}
@@ -330,7 +331,7 @@ void adminPage(vector<string> systemOrg, vector<string> systemUser)
 	}
 }
 
-void printOrganisationAdmin(string acc[], int orgSize)
+void viewOrganisationAdmin(string acc[], int orgSize)
 {
 	/*APIHandler apiHandler;
 	apiHandler.getAllOrgs("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE2NTEzOTQ4MTMsImlhdCI6MTY1MTMwODQxMywiaXNBZG1pbiI6MCwic3ViIjoiMSJ9.lFWn7AUKArrNMiqggBmnkOEHMt7sJFdshvN2JeCCtjw"); */
@@ -394,6 +395,118 @@ void printOrganisationAdmin(string acc[], int orgSize)
 
 	}
 }
+void viewOrganisationUser(string acc[], int orgSize)
+{
+	system("cls");
+
+	char key; // Key to be entered
+	int counter = 0, posy;
+
+	while (true)
+	{
+		gotoxy(2, 3); color(6); cout << "O R G A N I S A T I O N S"; color(7);
+		posy = 5;
+
+		for (int i = 0; i < orgSize; i++)
+		{
+			gotoxy(4, posy); cout << "-->";
+			i == counter ? color(6) : color(7);
+			gotoxy(9, posy); cout << acc[i];
+
+			posy += 2;
+		}
+
+		key = _getch();
+
+		if (key == 72 && (counter >= 1 && counter <= orgSize)) // 72 is the ASCII code for the up arrow
+		{
+			counter--;
+		}
+
+		if (key == 80 && (counter >= 0 && counter < orgSize - 1)) // 80 is the ASCII code for the down arrow
+		{
+			counter++;
+		}
+
+	}
+}
+
+void createOrganisation()
+{
+	system("cls");
+	char key = ' ';
+	int iPut = 0;
+	string info[5] = { "", "" };
+
+	do {
+		int colors[3] = { 7, 7 };
+		colors[iPut] = 6;
+		color(6);
+		gotoxy(26, 0); cout << "     _             _  __         _     _ _                          _     _ _      " << endl;
+		gotoxy(26, 1); cout << "  __| | __ _ ___  | |/ /   _  __| | __| | | ___ _ __ ___  _   _  __| | __| | | ___ " << endl;
+		gotoxy(26, 2); cout << " / _` |/ _` / __| | ' / | | |/ _` |/ _` | |/ _ \\ '_ ` _ \\| | | |/ _` |/ _` | |/ _ \\" << endl;
+		gotoxy(26, 3); cout << "| (_| | (_| \\__ \\ | . \\ |_| | (_| | (_| | |  __/ | | | | | |_| | (_| | (_| | |  __/" << endl;
+		gotoxy(26, 4); cout << " \\__,_|\\__,_|___/ |_|\\_\\__,_|\\__,_|\\__,_|_|\\___|_| |_| |_|\\__,_|\\__,_|\\__,_|_|\\___|" << endl;
+		gotoxy(15, 7); cout << "WWWWWWWWWWWWWWWWWWWWWWMWWMMWMMWWWWWWWWWWWWWWWWWWWWW";
+		gotoxy(15, 8); cout << "WWWWWWWWWWWWWWWWWWWWWNkdddddddKWWWWWWWWWWWWWWWWWWWW";
+		createInputField(8, " Course name", 40, 75, colors[0], info[0]);
+		gotoxy(15, 9); cout << "WWWWWWWWWWWWWWWWWWWWW0'       :OKNWWWWWWWWWWWWWWWWW";
+		gotoxy(15, 10); cout << "WWWWWWWWWWWWWWWWWWWWW0'         ;KWWWWWWWWWWWWWWWWW";
+		gotoxy(15, 11); cout << "WWWWWWWWWWWWWWWWWWWWW0' ;kOo.   ,KWWWWWWWWWWWWWWWWW";
+		gotoxy(15, 12); cout << "WWWWWWWWWWWWWWWWWWWWWO. .cokddddkNWWWWWWWWWWWWWWWWW";
+		createInputField(12, " Password   ", 40, 75, colors[1], info[1]);
+		gotoxy(15, 13); cout << "WWWWWWWWWWWWWWWWWWXkl'     .,lkXWWWWWWWWWWWWWWWWWWW";
+		gotoxy(15, 14); cout << "WWWWWWWWWWWWWWN0d:.            .:d0NWWWWWWWWWWWWWWW";
+		gotoxy(15, 15); cout << "WWWWWWWWWWWXkl,.                  .,lkXWWWWWWWWWWWW";
+		gotoxy(15, 16); cout << "WWWWWWWN0d:.                          .:d0NWWWWWWWW";
+		gotoxy(15, 17); cout << "WWWWWWNo.                                .oNWWWWWWW";
+		gotoxy(15, 18); cout << "WWWWWWNkl,    .c:.    ;lcll;    .:c.    ,lkNWWWWWWW";
+		gotoxy(15, 19); cout << "WWWWWWWWWx.   ;XK,   .OWWWMO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 20); cout << "WWWWWWWWWx.   ;XK,   .OWWWWO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 21); cout << "WWWWWWWWWx.   ;XK,   .OWWWWO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 22); cout << "WWWWWWWWWx.   ;XK,   .OWWWWO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 23); cout << "WWWWWWWWWx.   ;XK,   .OWWWWO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 24); cout << "WWWWWWWWWx.   ;XK,   .OWWWWO.   ,KX;   .xWWWWWWWWWW";
+		gotoxy(15, 25); cout << "WWWWWWWK0l    ,kx.   .o0OO0o.   .xk'    l0KWWWWWWWW";
+		gotoxy(15, 26); cout << "WWWWWNK:               .                  :KNWWWWWW";
+		gotoxy(15, 27); cout << "WWWWO;.                                    .;0WWWWW";
+		gotoxy(15, 28); cout << "WWWWXxodoododoodoodoooodooodoodoooodoodoododxXWWWWW";
+		gotoxy(15, 29); cout << "WWWWWWMWMWWMWWMWMMWMWWMWWMMWMWWMWWMWWMWMWWMWWWWWWWW";
+
+		if (iPut == 2) 
+		{
+			createButton(16, "  Create now  ", 14, 75, 2);
+		}
+		else {
+			createButton(16, "  Create now  ", 14, 75, 7);
+		}
+		
+		key = _getch();
+
+		if (key == '\r') 
+		{
+			iPut++;
+			key = ' ';
+		}
+
+		else 
+		{
+			if (iPut < 2) 
+			{
+				if (key == '\b')
+				{
+					info[iPut] = info[iPut].substr(0, info[iPut].size() - 1);
+				}
+
+				else
+				{
+					info[iPut] += key;
+				}
+			}
+		} 
+	} while (key != '\r' && iPut <= 2);
+}
+
 void displayQuestion()
 {
 	string info = "", question = "Who is the person who loves vscpi the most";
@@ -415,51 +528,6 @@ void displayQuestion()
 		}
 
 	} while (key != '\r');
-}
-void printOrganisationUser(string acc[], int orgSize)
-{
-	/*APIHandler apiHandler;
-	apiHandler.getAllOrgs("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE2NTEzOTQ4MTMsImlhdCI6MTY1MTMwODQxMywiaXNBZG1pbiI6MCwic3ViIjoiMSJ9.lFWn7AUKArrNMiqggBmnkOEHMt7sJFdshvN2JeCCtjw"); */
-	system("cls");
-
-
-	char key; // Key to be entered
-	int counter = 0;
-
-	while (true)
-	{
-		// Button for history notebook section
-		int posy = 5;
-		gotoxy(2, 3); color(6); cout << "O R G A N I S A T I O N S"; color(7);
-		for (int i = 0; i < orgSize; i++) {
-			gotoxy(4, posy); cout << "-->";
-			if (i == counter) {
-				color(6); gotoxy(9, posy); cout << acc[i];
-			}
-			else {
-				gotoxy(9, posy); cout << acc[i];
-				gotoxy(60, posy); color(8); cout << "Update | Delete";
-			}
-
-
-			posy += 2;
-			color(7);
-		}
-
-		key = _getch();
-
-		if (key == 72 && (counter >= 1 && counter <= orgSize)) // 72/75 is the ASCII code for the up arrow
-		{
-			counter--;
-		}
-
-		if (key == 80 && (counter >= 0 && counter < orgSize - 1)) // 80/77 is the ASCII code for the up arrow
-		{
-			counter++;
-		}
-
-
-	}
 }
 void displayNumber(int num, int posx, int posy)
 {
@@ -808,7 +876,7 @@ int Menu() //Main menu with three options
 				system("CLS");
 				string acc[5] = { "Vocational school of Programing", "United states of America",
 		"Kethering und scisorss", "Code block for Specialists", "Cooking restaurnat um France" };
-				printOrganisationAdmin(acc, 5);
+				viewOrganisationAdmin(acc, 5);
 			}
 
 			if (counter == 3)
@@ -848,9 +916,10 @@ int Menu() //Main menu with three options
 
 int main()
 {
-	vector<string>acc = { "Vocational school of Programing", "United states of America",
+	/*string acc[5] = { "Vocational school of Programing", "United states of America",
 		"Kethering und scisorss", "Code block for Specialists", "Cooking restaurnat um France" };
 	vector<string> users = { "Vesko Stoyanov", "Tereza Opanska", "KOstadin Tlg", "MC Stojan", "Yoana Smn" };
-	adminPage(acc, users);
+	viewOrganisationUser(acc, 5);*/
+	createOrganisation();
 
 }
